bbbf93f8840b52d5db406cff78b2096c177f02d7
Mahan Mehrabi <gnusmhn@gmail.com>
2025-09-22 16:15:16 +0330

    Update main.rs

--- 001. Print/src/main.rs
+++ 001. Print/src/main.rs
@@ -1,6 +1,7 @@
 // to get rid of errors about unused variables we simply write : #![allow(unused)] at the top of the program
 
 fn main() {
+    // println!("Hello, World!");
     print!("string 1"); // doesn't matter what comes after it (same line)
     println!("string 2"); // prints on a new line
     print!("string 3 \n"); // \n = new line
