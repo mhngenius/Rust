1ae72c7ae3b799ea813f77a13aa50967f5cb25e9
Mahan Mehrabi <gnusmhn@gmail.com>
2025-09-22 16:14:21 +0330

    Add files via upload

--- 001. Print/src/main.rs
+++ 001. Print/src/main.rs
@@ -1,3 +1,10 @@
+// to get rid of errors about unused variables we simply write : #![allow(unused)] at the top of the program
+
 fn main() {
-    println!("Hello, world!"); // println! is the correct form
+    print!("string 1"); // doesn't matter what comes after it (same line)
+    println!("string 2"); // prints on a new line
+    print!("string 3 \n"); // \n = new line
+    print!("string 4 \t"); // \t = Tab = 8 spaces
+    print!("Hi my name is \"Mahan\"\n"); // debugs the string problem
+    eprint!("This is an error !!!!!") // printing error
 }
